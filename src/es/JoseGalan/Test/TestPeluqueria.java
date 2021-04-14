bb47bd892018a2e548d1b56889b4568be6309acc
joseantoniogalan <llytodam@gmail.com>
2021-04-14 12:07:02 +0200

    Creacion Principal

--- src/es/JoseGalan/Test/TestPeluqueria.java
+++ src/es/JoseGalan/Test/TestPeluqueria.java
@@ -1,10 +1,16 @@
 package es.JoseGalan.Test;
 
+import es.JoseGalan.vistas.Login;
+import es.JoseGalanMaqueda.modelo.ModeloUsuarios;
+
 public class TestPeluqueria
 {
 
 	public static void main(String[] args)
 	{
+		Login login = new Login();
+		ModeloUsuarios modeloUsuarios = new ModeloUsuarios();
+		new ControladorUsuarios(login, modeloUsuarios);
 	}
 
 }
