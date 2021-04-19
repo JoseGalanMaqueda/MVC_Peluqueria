92a028c472d7c9126a9534fb5e5f7d391ee901e2
JoseGalanMaqueda <llytodam@gmail.com>
2021-04-19 21:26:57 +0200

    Correcion de faltas de ortografias

--- src/es/JoseGalanMaqueda/vistas/AltaTratamientos.java
+++ src/es/JoseGalanMaqueda/vistas/AltaTratamientos.java
@@ -15,7 +15,7 @@ public class AltaTratamientos
 		Frame frmAltaTratamientos = new Frame("Alta Tratamientos");
 		Label lblNombreAltaTratamiento = new Label("Nombre:");
 		Label lblPrecioAltaTratamiento = new Label("Precio:");
-		Label lblDescipcionAltaTratamiento = new Label("Descripcion:");
+		Label lblDescipcionAltaTratamiento = new Label("Descripción:");
 		TextField txtNombreAltaTratamiento = new TextField(20);
 		Color clFondo = new Color(204,229,255);
 		String[] precios = {"0.00","1.00","1.50","2.00","2.50","3.00","3.50","4.00","4.50","5.00","5.50","6.00","6.50","7.00","7.50","8.00","8.50","9.00","9.50"};
