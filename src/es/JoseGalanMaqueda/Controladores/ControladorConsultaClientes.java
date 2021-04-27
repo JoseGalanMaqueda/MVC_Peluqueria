8c4c9cf03eac8c3f554647b0e2ee98537ed01668
joseantoniogalan <llytodam@gmail.com>
2021-04-27 13:20:37 +0200

    Creacion Controlador Consulta Clientes

--- src/es/JoseGalanMaqueda/Controladores/ControladorConsultaClientes.java
+++ src/es/JoseGalanMaqueda/Controladores/ControladorConsultaClientes.java
@@ -1,5 +1,50 @@
 package es.JoseGalanMaqueda.Controladores;
 
-public class ControladorConsultaClientes {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+import es.JoseGalanMaqueda.modelo.ModeloCliente;
+import es.JoseGalanMaqueda.vistas.ConsultaClientes;
+
+public class ControladorConsultaClientes implements ActionListener, WindowListener {
+	
+	ConsultaClientes consultaClientes;
+	ModeloCliente modeloClientes;
+	
+	public ControladorConsultaClientes(ConsultaClientes consultaClientes, ModeloCliente modeloCliente) {
+		this.consultaClientes = consultaClientes;
+		this.modeloClientes = modeloCliente;
+		
+		this.consultaClientes.btnCancelarConsultaClientes.addActionListener(this);
+		this.consultaClientes.btnExportarPdfClientes.addActionListener(this);
+		this.consultaClientes.frmConsulaClientes.addWindowListener(this);
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void windowClosing(WindowEvent e) {}
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {}
+	
 }
