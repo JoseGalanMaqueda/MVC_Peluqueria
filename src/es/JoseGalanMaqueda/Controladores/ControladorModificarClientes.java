c7f4723501c47d0eb6ba38ceec39724f2f64f74a
joseantoniogalan <llytodam@gmail.com>
2021-05-06 13:39:09 +0200

    Funcionamiento correcto hasta ventana Edicion

--- src/es/JoseGalanMaqueda/Controladores/ControladorModificarClientes.java
+++ src/es/JoseGalanMaqueda/Controladores/ControladorModificarClientes.java
@@ -19,24 +19,56 @@ public class ControladorModificarClientes implements WindowListener, ActionListe
 		
 		// ======= VENTANA ELECCION CLIENTE =======
 		this.vistaModificarCliente.frmModificarClienteUno.addWindowListener(this);
+		this.vistaModificarCliente.btnBuscarCliente.addActionListener(this);
 		this.vistaModificarCliente.btnModificarUno.addActionListener(this);
 		this.vistaModificarCliente.btnCancelarModificarUno.addActionListener(this);
 		this.modeloCliente.cargarListadoClientes(this.vistaModificarCliente.choListaCliente);
+		
+		// ======= DIALOGO ERROR SELECCION CLIENTE ======
+		this.vistaModificarCliente.dlgErrorModificarSeleccionarCliente.addWindowListener(this);
+		
+		// ======= VENTANA EDICION CLIENTES =============
+		this.vistaModificarCliente.frmModificacionClientesDos.addWindowListener(this);
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void actionPerformed(ActionEvent e) 
+	{
+		if (e.getSource().equals(vistaModificarCliente.btnCancelarModificarUno)) 
+		{
+			vistaModificarCliente.frmModificarClienteUno.setVisible(false);
+		}
+		else if (e.getSource().equals(vistaModificarCliente.btnBuscarCliente)) 
+		{
+			modeloCliente.cargarListadoClientes(vistaModificarCliente.choListaCliente, vistaModificarCliente.txtBuscarCliente.getText());
+		}
+		else if (e.getSource().equals(vistaModificarCliente.btnModificarUno)) {
+			vistaModificarCliente.creacionDialogoNotificacion(vistaModificarCliente.dlgErrorModificarSeleccionarCliente, vistaModificarCliente.lblErrorModificarSeleccionarCliente);
+			if (!vistaModificarCliente.choListaCliente.getSelectedItem().equals("Selecciona un Cliente..")) 
+			{
+				vistaModificarCliente.cargarVentanaEdicionClientes();
+			}
+			else 
+			{
+				vistaModificarCliente.dlgErrorModificarSeleccionarCliente.setVisible(true);
+			}
+		}
 	}
 
 	@Override
 	public void windowOpened(WindowEvent e) {}
 
 	@Override
-	public void windowClosing(WindowEvent e) {
-		// TODO Auto-generated method stub
-		
+	public void windowClosing(WindowEvent e) 
+	{
+		if (vistaModificarCliente.frmModificarClienteUno.isActive()) 
+		{
+			vistaModificarCliente.frmModificarClienteUno.setVisible(false);
+		}
+		else if (vistaModificarCliente.dlgErrorModificarSeleccionarCliente.isActive()) 
+		{
+			vistaModificarCliente.dlgErrorModificarSeleccionarCliente.setVisible(false);
+		}
 	}
 
 	@Override
