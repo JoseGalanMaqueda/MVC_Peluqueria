36c2d6a2b40180e803f4e3cd8e9b13d7316505bb
joseantoniogalan <llytodam@gmail.com>
2021-05-07 13:16:36 +0200

    Actualizar Clientes completado

--- src/es/JoseGalanMaqueda/Controladores/ControladorModificarClientes.java
+++ src/es/JoseGalanMaqueda/Controladores/ControladorModificarClientes.java
@@ -12,10 +12,12 @@ public class ControladorModificarClientes implements WindowListener, ActionListe
 	
 	ModificarCliente vistaModificarCliente;
 	ModeloCliente modeloCliente;
+	String eleccionDos="";
 	
 	public ControladorModificarClientes(ModificarCliente vistaModificarCliente, ModeloCliente modeloCliente) {
 		this.vistaModificarCliente = vistaModificarCliente;
 		this.modeloCliente = modeloCliente;
+		
 		
 		// ======= VENTANA ELECCION CLIENTE =======
 		this.vistaModificarCliente.frmModificarClienteUno.addWindowListener(this);
@@ -29,6 +31,14 @@ public class ControladorModificarClientes implements WindowListener, ActionListe
 		
 		// ======= VENTANA EDICION CLIENTES =============
 		this.vistaModificarCliente.frmModificacionClientesDos.addWindowListener(this);
+		this.vistaModificarCliente.btnCancelarModificacionClientesDos.addActionListener(this);
+		this.vistaModificarCliente.btnModificacionClientesDos.addActionListener(this);
+		
+		// ======= DIALOGO ERROR MODIFICAR ================
+		this.vistaModificarCliente.dlgErrorModificarCliente.addWindowListener(this);
+		
+		// ======== DIALOGO MODIFICACION CORRECTA ===========
+		this.vistaModificarCliente.dlgClientesModificado.addWindowListener(this);
 	}
 
 	@Override
@@ -48,10 +58,36 @@ public class ControladorModificarClientes implements WindowListener, ActionListe
 			if (!vistaModificarCliente.choListaCliente.getSelectedItem().equals("Selecciona un Cliente..")) 
 			{
 				vistaModificarCliente.cargarVentanaEdicionClientes();
+				String[] datos = (modeloCliente.cargarDatosCliente(vistaModificarCliente.choListaCliente.getSelectedItem().split("-")[0])).split("-");
+				cargarDatosVentana(datos);
 			}
 			else 
 			{
 				vistaModificarCliente.dlgErrorModificarSeleccionarCliente.setVisible(true);
+			}
+		}
+		else if (e.getSource().equals(vistaModificarCliente.btnCancelarModificacionClientesDos)) 
+		{
+			vistaModificarCliente.frmModificacionClientesDos.setVisible(false);
+		}
+		else if (e.getSource().equals(vistaModificarCliente.btnModificacionClientesDos)) 
+		{
+			vistaModificarCliente.creacionDialogoNotificacion(vistaModificarCliente.dlgErrorModificarCliente, vistaModificarCliente.lblErrorModificarCliente);
+			if (modeloCliente.comprobacionDatosIntroducidos(vistaModificarCliente.txtNombreModificacionClientesDos,
+					vistaModificarCliente.txtApellidosModificacionClientesDos, vistaModificarCliente.txtDniModificacionClientesDos, vistaModificarCliente.txtDireccionModificacionClientesDos, eleccionDos))
+			{
+				if (modeloCliente.actualizarCliente(vistaModificarCliente.txtIdModificacionClientesDos,vistaModificarCliente.txtNombreModificacionClientesDos,
+						vistaModificarCliente.txtApellidosModificacionClientesDos, vistaModificarCliente.txtDniModificacionClientesDos, vistaModificarCliente.txtDireccionModificacionClientesDos, eleccionDos))
+				{
+					vistaModificarCliente.creacionDialogoNotificacion(vistaModificarCliente.dlgClientesModificado, vistaModificarCliente.lblModificadoCorrectamente);
+					vistaModificarCliente.dlgClientesModificado.setVisible(true);
+				}else {
+					vistaModificarCliente.lblConfirmacionModificarCliente.setText("Error al Insertar");
+					vistaModificarCliente.dlgErrorModificarCliente.setVisible(true);
+				}
+			}else {
+				vistaModificarCliente.lblConfirmacionModificarCliente.setText("Datos introducidos incorrectos");
+				vistaModificarCliente.dlgErrorModificarCliente.setVisible(true);
 			}
 		}
 	}
@@ -70,6 +106,20 @@ public class ControladorModificarClientes implements WindowListener, ActionListe
 		{
 			vistaModificarCliente.dlgErrorModificarSeleccionarCliente.setVisible(false);
 		}
+		else if (vistaModificarCliente.frmModificacionClientesDos.isActive()) 
+		{
+			vistaModificarCliente.frmModificacionClientesDos.setVisible(false);
+		}
+		else if (vistaModificarCliente.dlgErrorModificarCliente.isActive()) 
+		{
+			vistaModificarCliente.dlgErrorModificarCliente.setVisible(false);
+		}
+		else if (vistaModificarCliente.dlgClientesModificado.isActive()) 
+		{
+			vistaModificarCliente.dlgClientesModificado.setVisible(false);
+			vistaModificarCliente.frmModificacionClientesDos.setVisible(false);
+			vistaModificarCliente.frmModificarClienteUno.setVisible(false);
+		}
 	}
 
 	@Override
@@ -86,6 +136,24 @@ public class ControladorModificarClientes implements WindowListener, ActionListe
 
 	@Override
 	public void windowDeactivated(WindowEvent e) {}
+	
+	public void cargarDatosVentana(String[] datos) {
+		vistaModificarCliente.txtIdModificacionClientesDos.setText(datos[0]);
+		vistaModificarCliente.txtNombreModificacionClientesDos.setText(datos[1]);
+		vistaModificarCliente.txtApellidosModificacionClientesDos.setText(datos[2]);
+		vistaModificarCliente.txtDniModificacionClientesDos.setText(datos[3]);
+		vistaModificarCliente.txtDireccionModificacionClientesDos.setText(datos[4]);
+		if (datos[5].equals("Hombre")) 
+		{
+			vistaModificarCliente.chkHombreDos.setState(true);
+			eleccionDos = "Hombre";
+		}
+		else 
+		{
+			vistaModificarCliente.chkMujerDos.setState(true);
+			eleccionDos = "Mujer";
+		}
+	}
 	
 	
 }
